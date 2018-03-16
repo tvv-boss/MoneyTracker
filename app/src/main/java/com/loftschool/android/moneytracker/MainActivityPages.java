9f277552c692ec3c1d3791dc542ca9f389e1209d
TVV <tvv-boss@yandex.ru>
2018-03-16 13:35:10 +1100

    add end change activity_main_fragment.xml AndroidManifest.xml build.gradle colors.xml fragment_items.xml ItemsFragment.java MainActivityPages.java MainPagesAdapter.java strings.xml styles.xml

---
+++ app/src/main/java/com/loftschool/android/moneytracker/MainActivityPages.java
@@ -0,0 +1,46 @@
+package com.loftschool.android.moneytracker;
+
+/**
+ * Created by Vlad on 16.03.2018.
+ */
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivityPages extends AppCompatActivity {
+    private static final String TAG = "MainActivityPages";
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_fragment);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
+
+        MainPagesAdapter adapter = new MainPagesAdapter(getSupportFragmentManager(), this);
+        viewPager.setAdapter(adapter);
+
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+}
